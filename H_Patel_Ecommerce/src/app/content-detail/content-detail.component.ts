477d1ba3cd058313906ef11f658cc51b3a3a47d6
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-05 18:36:47 -0400

    details page is edited

--- H_Patel_Ecommerce/src/app/content-detail/content-detail.component.ts
+++ H_Patel_Ecommerce/src/app/content-detail/content-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Content } from '../models/content';
+import { PRODUCTService } from '../services/chess-champion.service';
 
 @Component({
   selector: 'app-content-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-detail.component.scss']
 })
 export class ContentDetailComponent implements OnInit {
+  contentItem?: Content;
+  id?: number;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private contentService: PRODUCTService) {
+  }
+  ngOnInit() {
+  
+    this.route.paramMap.subscribe(params => {
+      this.id = +(params.get('id') ?? 0); // uses the + unary operator
+
+      this.contentService.getContentItem(this.id).subscribe(singleItem => {
+        if (singleItem) {
+          this.contentItem = singleItem;
+        }
+        else {
+          this.router.navigate(['/contentNotFound']);
+        }
+      });
+    });
   }
 
 }
