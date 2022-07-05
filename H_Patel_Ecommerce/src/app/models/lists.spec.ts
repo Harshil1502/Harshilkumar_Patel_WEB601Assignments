76a38aa848bc88dc184c95b763619e45d3023b96
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-05 15:31:56 -0400

    Cleaning project

--- H_Patel_Ecommerce/src/app/models/lists.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { Lists } from './lists';
-
-describe('Lists', () => {
-  it('should create an instance', () => {
-    expect(new Lists()).toBeTruthy();
-  });
-});
