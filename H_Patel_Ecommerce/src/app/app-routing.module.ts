eee2fb040d534e13d1fcc62652fa689f3c9076f1
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-05 17:28:20 -0400

    Created working routes and Displayed all content

---
+++ H_Patel_Ecommerce/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/list",
+    pathMatch: "full"
+  },
+  {
+    path: "list",
+    component: ContentListComponent,
+  },
+  {
+    path: "detail/:id",
+    component: ContentDetailComponent
+  },
+  // {
+  //   path: "search",
+  //   component: ContentSearchComponent
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
