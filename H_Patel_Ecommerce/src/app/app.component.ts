26ba150f9397f0df8a2e5bf7c42f88079cb6de55
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-26 15:40:44 -0400

    Update app.component.ts

--- H_Patel_Ecommerce/src/app/app.component.ts
+++ H_Patel_Ecommerce/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DEFAULTPRODUCTCONTENT } from './data/mock-products';
+import { Content } from './models/content';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Harshilkumar Patel';
+  individualProduct: Content = DEFAULTPRODUCTCONTENT;
+  constructor(private productService: ProductService) {
 
+  }
+
+  ngOnInit(): void {
+    // getContentItem test
+      console.log("App component - Got the content item: ", productItem);
+      this.individualProduct = productItem;
+    });
+
+  }
+
+  checkForIdInList(id: string): void {
+    this.productService.getContentItem(Number(id)).subscribe(productItem => {
+      console.log("App component - Got the content item AGAIN: ", productItem);
+      this.individualProduct = productItem;
+    });
+  }
 }
