cfcf9a524c0307d2152582c80c2db5bd93a0d21a
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-26 11:17:53 -0400

    added Conent and updated version 0.3.0

--- H_Patel_Ecommerce/src/app/content-list/content-list.component.ts
+++ H_Patel_Ecommerce/src/app/content-list/content-list.component.ts
@@ -10,10 +10,10 @@ export class ContentListComponent implements OnInit {
 
   product: Content[];
   constructor() {
-    this.product = [{
+    this.product = [ {
       id: 0,
       type: 'Neck Band',
-      imageLink: '/assets/earphones_a_2.webp',
+      imageLink: 'https://th.bing.com/th/id/OIP.Qha6RA9qWTCU00joFAIuAQHaHL?w=192&h=187&c=7&r=0&o=5&dpr=1.25&pid=1.7',
       body: 'Very good platformer on the Nintendo Switch',
       title: 'Boat Rockers',
       price: 20,
@@ -21,39 +21,57 @@ export class ContentListComponent implements OnInit {
     },
     {
       id: 1,
-      type: 'Wired Earphone',
-      imageLink: '/assets/earphones_a_1.webp',
+      type: 'Headphone',
+      imageLink: 'https://images-na.ssl-images-amazon.com/images/I/81Fk-T0USaL.__AC_SX300_SY300_QL70_ML2_.jpg',
       body: 'Sound is so realistic',
-      title: 'Boat Rockers',
+      title: 'Gaming Headset',
       price: 90,
-      author: 'boat'
+      author: 'Pacrate'
     },
     {
       id: 2,
-      type: 'Cream Headphone',
-      imageLink: '/assets/headphones_c_3.webp',
+      type: '',
+      imageLink: 'https://store.sony.com.au/dw/image/v2/ABBC_PRD/on/demandware.static/-/Sites-sony-master-catalog/default/dw24ae300f/images/MDR1000XC/MDR1000XC.png',
       body: 'The best Noice cancelation ever happened',
-      title: 'Rockers 1',
+      title: 'Headphones',
       price: 100,
-      author: 'rockers'
+      author: 'Soney'
     }, 
     {
       id: 3,
-      type: 'White Headphone',
-      imageLink: '/assets/headphones_c_2.webp',
+      type: 'gaming',
+      imageLink: 'https://m.media-amazon.com/images/I/41H0C6y447L._AC_UY327_FMwebp_QL65_.jpg',
       body: 'Man This is a real beauty with the beast sound',
-      title: 'Rockers 2',
+      title: 'PS4 Headset',
       price: 170,
-      author: 'rockers'
+      author: 'Letton'
     }, 
     {
       id: 4,
-      type: 'Speakers',
-      imageLink: '/assets/speaker1.webp',
+      type: 'Speaker',
+      imageLink: 'https://th.bing.com/th/id/OIP.2iatXsqac9hciW7ffvzIuAHaHa?w=181&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7',
       body: 'With Surrounding sound ',
       title: 'Rockers pod',
       price: 100,
-      author: 'boat rockers'
+      author: 'boat'
+    },
+    {
+      id: 5,
+      type: 'Watch',
+      imageLink: 'https://m.media-amazon.com/images/I/71gg8mPlAuL._AC_UL480_FMwebp_QL65_.jpg',
+      body: 'best fitness gadget right now',
+      title: 'Apple',
+      price: 500,
+      author: 'Apple'
+    },
+    {
+      id: 6,
+      type: '',
+      imageLink: 'https://m.media-amazon.com/images/I/71KGrESMM3L._AC_UL480_FMwebp_QL65_.jpg',
+      body: ' fitness gadget',
+      title: 'Apple',
+      price: 450,
+      author: 'Apple'
     }];
 
 
